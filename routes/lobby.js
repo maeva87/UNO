6274ad4e4281a5c9aa9b9465a36b5841c208cba0
hjonatha <jonathan.hasard@ynov.com>
2026-04-03 12:26:07 +0200

    [ADD] adding something in lobby.js

---
+++ routes/lobby.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// À compléter par le membre A
+// GET /api/lobbies   ← liste des salles
+// POST /api/lobbies  ← créer une salle
+// POST /api/lobbies/:id/join ← rejoindre une salle
+
+module.exports = router;
