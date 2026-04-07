c93f95bb64610685f6f8905a82f92c95255e453b
hjonatha <jonathan.hasard@ynov.com>
2026-04-07 10:22:29 +0200

    [FIX] fixing and adding new socket.js

---
+++ socket.js
@@ -0,0 +1,10 @@
+let io;
+
+module.exports = {
+  init: (server) => {
+    const { Server } = require('socket.io');
+    io = new Server(server, { cors: { origin: '*' } });
+    return io;
+  },
+  getIO: () => io
+};
